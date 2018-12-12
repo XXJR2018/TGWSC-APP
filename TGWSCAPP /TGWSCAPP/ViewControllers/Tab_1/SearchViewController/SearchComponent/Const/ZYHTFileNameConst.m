84713644d50c8a21f6b9b47d17c9e74d73e43613
XXJR2018 <3525049291@qq.com>
2018-12-12 16:27:29 +0800

    NSString *const NearByShopSearchShopNameHistoryCacheFileName = @"NearByShopSearchShopNameHistoryCacheFileName"; NSString *const NearByShopSearchMapAddressHistoryCacheFileName = @"NearByShopSearchShopNameHistoryCacheFileName"; NSString *const AllShopSearchHistoryCacheFileName = @"NearByShopSearchShopNameHistoryCacheFileName";

--- TGWSCAPP /TGWSCAPP/ViewControllers/Tab_1/SearchViewController/SearchComponent/Const/ZYHTFileNameConst.m
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_1/SearchViewController/SearchComponent/Const/ZYHTFileNameConst.m
@@ -3,8 +3,13 @@
 
 #import "ZYHTFileNameConst.h"
 
-NSString *const NearByShopSearchShopNameHistoryCacheFileName = @"NearByShopSearchShopNameHistoryCacheFileName";
-NSString *const NearByShopSearchMapAddressHistoryCacheFileName = @"NearByShopSearchMapAddressHistoryCacheFileName";
-NSString *const AllShopSearchHistoryCacheFileName = @"AllShopSearchHistoryCacheFileName";
+//NSString *const NearByShopSearchShopNameHistoryCacheFileName = @"NearByShopSearchShopNameHistoryCacheFileName";
+//NSString *const NearByShopSearchMapAddressHistoryCacheFileName = @"NearByShopSearchMapAddressHistoryCacheFileName";
+//NSString *const AllShopSearchHistoryCacheFileName = @"AllShopSearchHistoryCacheFileName";
 
-//NSString *const CYPasswordViewEnabledUsen = @"SDDDDFDFDFDFDFDFDFFDFDF";
+
+NSString *const NearByShopSearchShopNameHistoryCacheFileName = @"NearByShopSearchShopNameHistoryCacheFileName";
+NSString *const NearByShopSearchMapAddressHistoryCacheFileName = @"NearByShopSearchShopNameHistoryCacheFileName";
+NSString *const AllShopSearchHistoryCacheFileName = @"NearByShopSearchShopNameHistoryCacheFileName";
+
+

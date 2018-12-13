5e357f62e47c1beac249a68585e5587a0ffa6863
xxjr03 <ioschangnan@163.com>
2018-12-13 17:58:33 +0800

    分类页面部分代码提交

--- TGWSCAPP /TGWSCAPP/ViewControllers/Tab_2/TabViewController_2.m
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_2/TabViewController_2.m
@@ -10,7 +10,11 @@
 
 @interface TabViewController_2 ()
 {
-    NSMutableArray *_sortFirstArr;
+    UIScrollView *_scView;
+    UIButton *_sortFirstBtn;
+    UIView *_sortFristView;
+    NSMutableArray *_sortFirstTitleArr;
+    NSMutableArray *_sortFirstBtnArr;
 }
 @end
 
@@ -30,16 +34,64 @@
     [super viewDidLoad];
     self.hideBackButton = YES;
     [self layoutNaviBarViewWithTitle:@"分类"];
-    
-    
+    _sortFirstBtnArr = [NSMutableArray array];
+    _sortFirstTitleArr = [NSMutableArray arrayWithArray:@[@"冬季专区",@"爆品专区",@"新品专区",@"居家",@"鞋包配饰",@"服装",@"洗护",@"饮食",@"母婴",@"餐厨",@"保健",@"文体",@"12.12专区",@"特色区"]];
     [self layoutUI];
 }
 
 -(void)layoutUI{
     
+    _scView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, NavHeight, 100, SCREEN_HEIGHT - NavHeight - TabbarHeight)];
+    [self.view addSubview:_scView];
+    _scView.backgroundColor = [UIColor whiteColor];
+    _scView.bounces = NO;
+    _scView.pagingEnabled = NO;
+    _scView.showsVerticalScrollIndicator = NO;
+    
+    UIView *viewX = [[UIView alloc]initWithFrame:CGRectMake(CGRectGetMaxX(_scView.frame) - 0.5, 0, 0.5, CGRectGetMaxY(_scView.frame))];
+    [_scView addSubview:viewX];
+    viewX.backgroundColor = [ResourceManager color_5];
+    
+    for (int i = 0; i < _sortFirstTitleArr.count; i ++) {
+        _sortFirstBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 50 * i, 100, 50)];
+        [_scView addSubview:_sortFirstBtn];
+        _sortFirstBtn.tag = i + 100;
+        [_sortFirstBtn setTitle:_sortFirstTitleArr[i] forState:UIControlStateNormal];
+        [_sortFirstBtn setTitleColor:[ResourceManager color_1] forState:UIControlStateNormal];
+        [_sortFirstBtn setTitleColor:[ResourceManager redColor1] forState:UIControlStateSelected];
+        _sortFirstBtn.titleLabel.font = [UIFont systemFontOfSize:14];
+        [_sortFirstBtn addTarget:self action:@selector(sortFirstTouch:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [_sortFirstBtnArr addObject:_sortFirstBtn];
+        
+    }
+    
+    _scView.contentSize =CGSizeMake(0, CGRectGetMaxY(_sortFirstBtn.frame) + 10);
+    ((UIButton *)_sortFirstBtnArr[0]).selected = YES;
+    _sortFristView = [[UIView alloc]initWithFrame:CGRectMake(0, (50 - 20)/2, 2, 20)];
+    [_scView addSubview:_sortFristView];
+    _sortFristView.backgroundColor = [ResourceManager redColor1];
+    
 }
 
-
+#pragma mark-一级菜单点击事件
+-(void)sortFirstTouch:(UIButton *)sender{
+    
+    if (sender == _sortFirstBtn) {
+        return;
+    }
+    NSLog(@"%@",_sortFirstTitleArr[sender.tag - 100]);
+    ((UIButton *)_sortFirstBtnArr[0]).selected = NO;
+    if (sender != _sortFirstBtn) {
+        _sortFirstBtn.selected = NO;
+        _sortFirstBtn = sender;
+    }else{
+        //避免重复点击
+        return;
+    }
+    _sortFirstBtn.selected = YES;
+    _sortFristView.frame = CGRectMake(0, (50 - 20)/2 + (sender.tag - 100) * 50, 2, 20);
+}
 
 -(void)addButtonView{
     [self.view addSubview:self.tabBar];

14fa99396a0a08fc9ac9ea67f25719d2f3768a17
xxjr03 <ioschangnan@163.com>
2018-12-25 16:36:39 +0800

    修改个人中心文件名

---
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_4/UserInfo/AccountSecurity/AccountSecurityViewController.h
@@ -0,0 +1,17 @@
+//
+//  AccountSecurityViewController.h
+//  TGWSCAPP
+//
+//  Created by xxjr03 on 2018/12/25.
+//  Copyright © 2018 xxjr03. All rights reserved.
+//
+
+#import "CommonViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AccountSecurityViewController : CommonViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_4/UserInfo/NickNameViewController.h
@@ -0,0 +1,19 @@
+//
+//  NickNameViewController.h
+//  TGWSCAPP
+//
+//  Created by xxjr03 on 2018/12/24.
+//  Copyright © 2018 xxjr03. All rights reserved.
+//
+
+#import "CommonViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NickNameViewController : CommonViewController
+
+@property(nonatomic, copy)Block_String nickNameBlock;
+
+@end
+
+NS_ASSUME_NONNULL_END

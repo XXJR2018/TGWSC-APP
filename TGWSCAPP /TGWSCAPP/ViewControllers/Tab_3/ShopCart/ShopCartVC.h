f61cbded99ed7aaf6c3d13d18a387290397ca86e
XXJR2018 <3525049291@qq.com>
2018-12-29 14:48:32 +0800

    1, 加入购物车页面 2，首页加入刷新效果

---
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_3/ShopCart/ShopCartVC.h
@@ -0,0 +1,17 @@
+//
+//  ShopCartVC.h
+//  TGWSCAPP
+//
+//  Created by xxjr02 on 2018/12/29.
+//  Copyright © 2018 xxjr03. All rights reserved.
+//
+
+#import "CommonViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShopCartVC : CommonViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_3/ShopCart/ShopCartVC.m
@@ -0,0 +1,24 @@
+//
+//  ShopCartVC.m
+//  TGWSCAPP
+//
+//  Created by xxjr02 on 2018/12/29.
+//  Copyright © 2018 xxjr03. All rights reserved.
+//
+
+#import "ShopCartVC.h"
+
+@interface ShopCartVC ()
+
+@end
+
+@implementation ShopCartVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end

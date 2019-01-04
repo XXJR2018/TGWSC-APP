c45f5dfa94be2acb6f2d0178f5b1cee210d6fb44
XXJR2018 <3525049291@qq.com>
2019-01-04 09:43:34 +0800

    1,提交修改。

--- TGWSCAPP /TGWSCAPP/ViewControllers/Tab_3/OrderDetial/OrderDetialVC.m
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_3/OrderDetial/OrderDetialVC.m
@@ -20,14 +20,6 @@
     [self layoutNaviBarViewWithTitle:@"确认订单"];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end

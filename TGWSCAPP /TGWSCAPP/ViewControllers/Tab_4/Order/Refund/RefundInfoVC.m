e1bb6a2b92e7e097074bc0d74690b35a428d6112
XXJR2018 <3525049291@qq.com>
2019-01-11 18:11:55 +0800

    1, 提交代码。

--- TGWSCAPP /TGWSCAPP/ViewControllers/Tab_4/Order/Refund/RefundInfoVC.m
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_4/Order/Refund/RefundInfoVC.m
@@ -9,7 +9,16 @@
 #import "RefundInfoVC.h"
 
 @interface RefundInfoVC ()
-
+{
+    UIScrollView *scView;
+    
+    UIView *viewHead; // 退款头部
+    
+    UIView *viewShop;  // 退款商品信息
+    
+    
+    NSDictionary *dicUI;
+}
 @end
 
 @implementation RefundInfoVC
@@ -18,18 +27,110 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self layoutNaviBarViewWithTitle:@"退款详情"];
+
+    [self initData];
     
     [self getUIDataFromWeb];
     
 }
 
+-(void) initData
+{
+    dicUI = [[NSDictionary alloc] init];
+}
 
 #pragma mark --- 布局UI
 -(void) layoutUI:(NSDictionary*) dicValue
 {
-    //处理状态(0-待处理 1-商家待收货 2-同意退款 3-退款成功 4-商家拒绝退款 5-交易关闭 6-可重新申请)
+    scView = [[UIScrollView alloc]initWithFrame:CGRectMake(0.f, NavHeight, SCREEN_WIDTH, SCREEN_HEIGHT - NavHeight - 100)];
+    [self.view addSubview:scView];
+    scView.contentSize = CGSizeMake(0, 500);
+    scView.pagingEnabled = NO;
+    scView.bounces = NO;
+    scView.showsVerticalScrollIndicator = FALSE;
+    scView.showsHorizontalScrollIndicator = FALSE;
+    scView.backgroundColor = [ResourceManager viewBackgroundColor];
+    
+    [self layoutHead:dicValue];
+    
 }
 
+
+-(void) layoutHead:(NSDictionary*) dicValue
+{
+    viewHead = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 130)];
+    [scView addSubview:viewHead];
+    viewHead.backgroundColor = [UIColor whiteColor];
+    
+    UIView *viewFG = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 1)];
+    [viewHead addSubview:viewFG];
+    viewFG.backgroundColor = [ResourceManager color_5];
+    
+    
+    int iTopY = 15;
+    int iLeftX = 10;
+    UILabel *labelStatus = [[UILabel alloc] initWithFrame:CGRectMake(iLeftX, iTopY, SCREEN_WIDTH - 2*iLeftX, 20)];
+    [viewHead addSubview:labelStatus];
+    labelStatus.textColor = [ResourceManager priceColor];
+    labelStatus.font = [UIFont systemFontOfSize:14];
+    labelStatus.text = dicValue[@"serverStatusDesc"];
+    
+    
+    iTopY += labelStatus.height + 10;
+    UILabel *labelDesc = [[UILabel alloc] initWithFrame:CGRectMake(iLeftX, iTopY, SCREEN_WIDTH - 2*iLeftX, 46)];
+    [viewHead addSubview:labelDesc];
+    //labelDesc.backgroundColor = [UIColor yellowColor];
+    labelDesc.textColor = [ResourceManager color_1];
+    labelDesc.font = [UIFont systemFontOfSize:12];
+    labelDesc.numberOfLines = 0;
+    labelDesc.text = @"";
+    
+    int iServerStatus = [dicValue[@"serverStatus"] intValue];
+    //iServerStatus = 3;
+    //处理状态(0-待处理 1-商家待收货 2-同意退款（需要填写物流） 3-退款成功 4-商家拒绝退款 5-交易关闭 6-可重新申请)
+    if (0 == iServerStatus)
+     {
+        labelStatus.text = @"等待商家处理退款申请";
+        labelDesc.text = dicValue[@"serverDesc"]? dicValue[@"serverDesc"]:@"如果商家同意:退款金额将原路返回支付账号\n如果商家拒绝并已经发货：请联系商家沟通后再次提交申请";
+     }
+    else if (1 == iServerStatus)
+     {
+        labelStatus.text = @"商家等待收货";
+        labelDesc.text = dicValue[@"serverDesc"];
+     }
+    else if (2 == iServerStatus)
+     {
+        labelStatus.text = @"商家同意退款，请退货给商家";
+        labelDesc.text = dicValue[@"receiveAddrDtl"]? [NSString stringWithFormat:@"收货地址： %@", dicValue[@"receiveAddrDtl"]]:@"请和商家联系，获取退货地址";;
+     }
+    else if (3 == iServerStatus)
+     {
+        labelStatus.text = @"退款成功";
+        
+        NSString *strText =  [NSString stringWithFormat:@"退款时间:%@\n退款金额:%@\n退款金额将按原路返回支付账户。",dicValue[@"createTime"],dicValue[@"refundTotalAmt"]];
+        
+        labelDesc.text = strText;
+     }
+    else if (4 == iServerStatus)
+     {
+        labelStatus.text = @"商家拒绝退款";
+        labelDesc.text =  dicValue[@"serverDesc"]? [NSString stringWithFormat:@"%@", dicValue[@"serverDesc"]]:@"请与商家沟通交流后再次提交申请";
+     }
+    else if (5 == iServerStatus)
+     {
+        labelStatus.text = @"退款关闭";
+        labelDesc.text = dicValue[@"createTime"]? [NSString stringWithFormat:@"%@", dicValue[@"createTime"]]:@"";
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
 
 #pragma mark  ---  网络请求
 -(void)getUIDataFromWeb{
@@ -59,10 +160,10 @@
     
     if (1000 == operation.tag)
      {
-        NSDictionary *dicUI = operation.jsonResult.attr;
+        dicUI = operation.jsonResult.attr;
         if (dicUI)
          {
-            
+            [self layoutUI:dicUI];
          }
      }
     

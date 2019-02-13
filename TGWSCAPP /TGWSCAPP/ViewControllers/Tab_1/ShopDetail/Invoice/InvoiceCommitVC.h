a05c46053186b1d20660f8ca01f6d8abf3d4af0b
XXJR2018 <3525049291@qq.com>
2019-02-13 14:09:07 +0800

    1, 加入电子发票 相关信息。

---
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_1/ShopDetail/Invoice/InvoiceCommitVC.h
@@ -0,0 +1,17 @@
+//
+//  InvoiceCommitVC.h
+//  TGWSCAPP
+//
+//  Created by xxjr02 on 2019/2/13.
+//  Copyright © 2019 xxjr03. All rights reserved.
+//
+
+#import "CommonViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InvoiceCommitVC : CommonViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_1/ShopDetail/Invoice/InvoiceInfoVC.h
@@ -0,0 +1,17 @@
+//
+//  InvoiceInfoVC.h
+//  TGWSCAPP
+//
+//  Created by xxjr02 on 2019/2/13.
+//  Copyright © 2019 xxjr03. All rights reserved.
+//
+
+#import "CommonViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InvoiceInfoVC : CommonViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

2bbe75cc459db5a24ffc35461804e1871624faca
xxjr03 <ioschangnan@163.com>
2019-02-13 11:28:15 +0800

    客服

---
+++ TGWSCAPP /TGWSCAPP/ViewControllers/Tab_4/Appraise/AppraiseViewController.m
@@ -0,0 +1,56 @@
+//
+//  AppraiseViewController.m
+//  TGWSCAPP
+//
+//  Created by xxjr03 on 2019/2/13.
+//  Copyright © 2019 xxjr03. All rights reserved.
+//
+
+#import "AppraiseViewController.h"
+
+@interface AppraiseViewController ()
+
+@end
+
+@implementation AppraiseViewController
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [MobClick beginLogPageView:@"评价"];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [MobClick endLogPageView:@"评价"];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self layoutNaviBarViewWithTitle:@"评价"];
+    
+//    _tableView.backgroundColor = [UIColor whiteColor];
+//    [_tableView setTableFooterView:[UIView new]];
+//    [_tableView registerClass:[BalanceViewCell class] forCellReuseIdentifier:@"balance_cell"];
+//    [_tableView setSeparatorInset:UIEdgeInsetsMake(0, -20, 0, 0)];
+//    [_tableView setSeparatorColor:[ResourceManager color_5]];
+    
+    [self layoutUI];
+}
+
+-(void)layoutUI{
+
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
